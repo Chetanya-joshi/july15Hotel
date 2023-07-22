9c6fcbc739848811f175722a424678795f6a0f0b
Chetanya-joshi <108217245+Chetanya-joshi@users.noreply.github.com>
2023-07-22 14:54:25 +0530

    Update TheCityJoy.js

--- src/components/TheCityJoy.js
+++ src/components/TheCityJoy.js
@@ -13,6 +13,8 @@ import durgaMa from '../assets/durgaMa.jpg';
 import './TheCityJoy.css';
 import Header from './LandingPAge/Header';
 import {Link} from 'react-router-dom';
+import Footer from "./LandingPAge/Footer";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 
 export default function TheCityJoy() {
@@ -58,7 +60,7 @@ export default function TheCityJoy() {
     <div className="App">
       <Slider {...settings}>
         <Link to='/temple'>
-      <div className="card" >
+      <div className="card1" >
         <div className="card-top">
           <img src={temple} alt="" />
           <h1 >Japanese Temple in Kolkata - Nipponzan Myohoji Buddhist Temple
@@ -73,7 +75,7 @@ export default function TheCityJoy() {
       </Link>
 
 <Link to="/rabindralake">
-      <div className="card">
+      <div className="card1">
         <div className="card-top">
           <img src={RabindraLake} alt="" />
           <h1>Kolkata’s fitness and hangout spot - Rabindra Sarobar Lake</h1>
@@ -86,7 +88,7 @@ export default function TheCityJoy() {
       </Link>
 
 <Link to="/museum">
-      <div className="card">
+      <div className="card1">
         <div className="card-top">
           <img src={Alipore} alt="" />
           <h1>Newly arrived tourist spot in Kolkata -Alipore Jail Museum</h1>
@@ -99,7 +101,7 @@ export default function TheCityJoy() {
       </Link>
 
       <Link to="/food">
-      <div className="card">
+      <div className="card1">
         <div className="card-top">
           <img src={food} alt="" />
           <h1>Top 5 street food places in Kolkata.</h1>
@@ -113,7 +115,7 @@ export default function TheCityJoy() {
 
       <Link to="/mahalya">
 
-      <div className="card">
+      <div className="card1">
         <div className="card-top">
           <img src={god} alt="" />
           <h1>Cultural significance of Mahalaya.</h1>
@@ -127,7 +129,7 @@ export default function TheCityJoy() {
 
       <Link to="/durgapooja">
 
-      <div className="card">
+      <div className="card1">
         <div className="card-top">
           <img src={durgaMa} alt="" />
           <h1>The biggest festival of West Bengal - Durga Puja</h1>
@@ -146,6 +148,78 @@ export default function TheCityJoy() {
       
       </Slider>
     </div>
+    <Container className="py-5">
+          <h4 className="pt-4" style={{ textAlign: "center" }}>
+            Our Latest Blogs{" "}
+          </h4>
+          <hr />
+          <Row className="pt-5">
+            <Col xs={12} sm={6} md={3} lg={3}>
+              <Link to="/durgapooja">
+                <Card style={{ backgroundColor: "aliceblue", border: "none" }}>
+                  <Card.Img variant="top" src={durgaMa} alt="Card 1" />
+                  <Card.Body>
+                    {/* <Card.Title></Card.Title> */}
+                    <Card.Text className="py-3" style={{ color: "teal" }}>
+                      The biggest festival of West Bengal - Durga Puja .....
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Link to="/rabindralake">
+                <Card style={{ backgroundColor: "aliceblue", border: "none" }}>
+                  <Card.Img
+                    variant="top"
+                    src={RabindraLake}
+                    alt="Card 2"
+                    style={{ height: "170px", overflow: "hidden" }}
+                  />
+                  <Card.Body>
+                    {/* <Card.Title>Card 2</Card.Title> */}
+                    <Card.Text className="py-3" style={{ color: "teal" }}>
+                      Kolkata’s fitness and hangout spot - Rabindra Sarobar Lake
+                      . . . .
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Link to="/food">
+                <Card style={{ backgroundColor: "aliceblue", border: "none" }}>
+                  <Card.Img
+                    variant="top"
+                    src={food}
+                    alt="Card 3"
+                    style={{ height: "170px", overflow: "hidden" }}
+                  />
+                  <Card.Body>
+                    {/* <Card.Title>Card 3</Card.Title> */}
+                    <Card.Text className="py-3" style={{ color: "teal" }}>
+                      Top 5 street food places in Kolkata to Explore. . . . . .
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Link to="/mahalya">
+                <Card style={{ backgroundColor: "aliceblue", border: "none" }}>
+                  <Card.Img variant="top" src={god} alt="Card 4" />
+                  <Card.Body>
+                    {/* <Card.Title>Card 4</Card.Title> */}
+                    <Card.Text className="py-3" style={{ color: "teal" }}>
+                      Cultural significance of Mahalaya.. . . . . .
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
     </div>
     </>
   )
